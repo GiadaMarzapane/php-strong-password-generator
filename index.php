22882ece6058501d2ed24859c69d5968db91b09b
GiadaMarzapane <giada.ortesta@gmail.com>
2023-02-22 15:50:56 +0100

    Add controlli

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
 <?php
-    function generatePassword($arg) {
-        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!"#$%&(){|}*+,-./:;<=>?@[\]^_~';
-        $password = [];
-        $alphaLength = strlen($alphabet) - 1;
-        for ($i = 0; $i < $arg; $i++) {
-            $n = rand(0, $alphaLength);
-            $password[] = $alphabet[$n];
-        }
-        return implode($password);
-    };
+$error = null;
 
-    $passlength = $_GET['pass-length'];
+if ((isset($_GET['pass-length'])) && (is_numeric($_GET['pass-length']))) {
+    $passlength = intval($_GET['pass-length']);
+
+    if (($passlength < 8) || ($passlength > 32)) {
+        $error = 'La password deve avere minimo 8 caratteri e massimo 32';
+    } else {
+        function generatePassword($arg)
+        {
+            $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!"#$%&(){|}*+,-./:;<=>?@[\]^_~';
+            $password = [];
+            $alphaLength = strlen($alphabet) - 1;
+            for ($i = 0; $i < $arg; $i++) {
+                $n = rand(0, $alphaLength);
+                $password[] = $alphabet[$n];
+            }
+            return implode($password);
+        };
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -33,9 +43,13 @@
             <button type="submit">Genera</button>
         </form>
         <p>
-            <?php
-            echo $passlength
-            ?>
+            <strong>
+                <?php
+                if ($error != null) {
+                    echo $error;
+                }
+                ?>
+            </strong>
         </p>
         <h3>La tua password generata è:</h3>
         <p>
