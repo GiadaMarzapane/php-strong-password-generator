7ceb2c9411a625dc756ae0c0e6805e09fdae761f
GiadaMarzapane <giada.ortesta@gmail.com>
2023-02-22 16:00:39 +0100

    Creato file functions.php e incluso in index.php

--- index.php
+++ index.php
@@ -1,23 +1,18 @@
 <?php
+// includo file per la functions creata
+include __DIR__ . './functions.php';
+
+// variabile creata per controlli php
 $error = null;
 
+// controlli php --> inserito valore numerico - lunghezza caratteri
 if ((isset($_GET['pass-length'])) && (is_numeric($_GET['pass-length']))) {
     $passlength = intval($_GET['pass-length']);
 
     if (($passlength < 8) || ($passlength > 32)) {
         $error = 'La password deve avere minimo 8 caratteri e massimo 32';
     } else {
-        function generatePassword($arg)
-        {
-            $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!"#$%&(){|}*+,-./:;<=>?@[\]^_~';
-            $password = [];
-            $alphaLength = strlen($alphabet) - 1;
-            for ($i = 0; $i < $arg; $i++) {
-                $n = rand(0, $alphaLength);
-                $password[] = $alphabet[$n];
-            }
-            return implode($password);
-        };
+        generatePassword($arg);
     }
 }
 
